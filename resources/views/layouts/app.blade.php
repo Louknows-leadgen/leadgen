058a24c606c224335249c8a3f1341b23ff79aee1
Lourence John <lourencejohn@digicononline.com>
2020-02-05 01:23:43 +0800

    added home menu on the register page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,12 +44,14 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('root') }}">Home</a>
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
-
+                                    
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
