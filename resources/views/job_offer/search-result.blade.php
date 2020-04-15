cebb467c3a9e6d4d9bd7c1efa575e48c0c6b46bd
LourenceI3Laptop <lourence.johnc@gmail.com>
2020-04-16 05:59:43 +0800

    fix button misalignment

--- resources/views/job_offer/search-result.blade.php
+++ resources/views/job_offer/search-result.blade.php
@@ -9,9 +9,9 @@
 			  		<div class="col-md-10">
 			  			<h5>{{ implode(' ',[$applicant->first_name,$applicant->last_name]) }}</h5>
 			  			<p><span class="fa fa-briefcase text-muted"></span> {{$applicant->job_name}}</p>
-			  			<a href="{{ route('employees.create',['applicant_id'=>$applicant->applicant_id]) }}" class="btn btn-primary mr-2">Hire</a>
-			  			<span data-id="{{ $applicant->applicant_id }}" class="btn btn-secondary remove-trigger mr-2">No show</span>
-			  			<span data-id="{{ $applicant->applicant_id }}" class="btn btn-danger decline-offer-trig mr-2">Declined Offer</span>
+			  			<a href="{{ route('employees.create',['applicant_id'=>$applicant->applicant_id]) }}" class="btn btn-primary mb-2 mr-2">Hire</a>
+			  			<span data-id="{{ $applicant->applicant_id }}" class="btn btn-secondary remove-trigger mb-2 mr-2">No show</span>
+			  			<span data-id="{{ $applicant->applicant_id }}" class="btn btn-danger decline-offer-trig mb-2 mr-2">Declined Offer</span>
 			  			<a href="{{ route('blacklists.blacklist_applicant',['id'=>$applicant->applicant_id]) }}" class="btn btn-dark blacklist-trig mr-2 mb-2">
 			  				Blacklist
 			  			</a>
